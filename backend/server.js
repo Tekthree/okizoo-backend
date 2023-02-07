568ee1cd006a673aa76e1af91a411d803509f192
Tek Jones <tekthree@gmail.com>
2023-02-06 17:50:31 -0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+
+app.use('/api/urls', require('./routes/urlRoutes'));
+
+
+app.listen(port, () => console.log(`Currently listening on port ${port}`));
