a3c11b004d1407f111ef19865cec1d7f5663635d
Tek Jones <tekthree@gmail.com>
2023-02-06 22:30:49 -0800

    adds controller for url routes

---
+++ backend/controllers/urlsController.js
@@ -0,0 +1,12 @@
+// description = Get Urls
+// route - Get /api/url
+// access - Private
+
+
+const getUrls = (req,res)=>{
+   res.status(200).json({ message: 'Get Url' });
+}
+
+module.exports ={
+  getUrls,
+}
--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {getUrls} = require('../controllers/urlsController.js')
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Get goals' });
+router.get('/', getUrls);
+router.post('/', (req, res) => {
+  res.status(200).json({ message: 'Set Url' });
 });
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Update Url ${req.params.id}` });
+});
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `Delete Url ${req.params.id}` });
+});
+
 
 module.exports = router;
