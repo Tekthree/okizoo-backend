3bc7eefe3299b3aebc35f1545dea317deeacfdc7
Tek Jones <tekthree@gmail.com>
2024-07-08 14:43:47 -0700

    back to home dev stat

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,6 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-
 app.use('/api/urls', require('./routes/urlRoutes'));
 
-
 app.listen(port, () => console.log(`Currently listening on port ${port}`));
-
-
